3759ec3c8a4821aa5c13544474660072aa018d8c
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-28 21:24:31 +0200

    Started testing with the new manager layouts, seems to work quite well so far, next step is re-implement the trading strategy with the new pattern, and then optimize, and then make reports, then the very basic form of this project is already done

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import datetime as dt
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from traderbot.constants import _CandleTimeframe, _Symbol
+from traderbot.data_manager import DatabaseDataManager
 from traderbot.data_manager import main as dm_main
+from traderbot.db_manager import DatabaseManager
+from traderbot.ema_module import computeEMA
 from traderbot.logger import setup_logger
 
 
-def main():
+def fill_db():
     setup_logger()
     dm_main()
+
+
+def main():
+    with DatabaseManager().manage_connection() as conn:
+        ddm = DatabaseDataManager(conn)
+        candles = ddm.get_candles(
+            _Symbol.EURUSD,
+            _CandleTimeframe.H1,
+            dt.datetime(2023, 10, 1, tzinfo=dt.timezone.utc),
+        )
+    print(candles)
 
 
 if __name__ == "__main__":
