3144bf4556fbc8c45103eb1d845c1b9c54b1d16e
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-28 19:49:42 +0200

    Adjusted comments and function arg names

--- Traderlib/ema_module.cpp
+++ Traderlib/ema_module.cpp
@@ -1,26 +1,25 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>      // Needed to convert std::vector to Python list
-#include <pybind11/numpy.h>    // Needed to handle NumPy arrays
+#include <pybind11/stl.h>   // Needed to convert std::vector to Python list
+#include <pybind11/numpy.h> // Needed to handle NumPy arrays
 #include <vector>
 #include <stdexcept>
 
-std::vector<float> computeEMA(const pybind11::array_t<float> &data_array, int span)
+std::vector<float> computeEMA(const pybind11::array_t<float> &data_array, int lookback)
 {
-    if (span <= 0)
+    if (lookback <= 0)
     {
-        throw std::invalid_argument("Span must be a positive integer.");
+        throw std::invalid_argument("Lookback must be a positive integer.");
     }
 
-    // Get the buffer info from the NumPy array
+    // Extracts the buffer from the np array
     pybind11::buffer_info buf = data_array.request();
 
-    // Ensure the input is a 1D array
+    // Checks that the numpy array is flat
     if (buf.ndim != 1)
     {
         throw std::runtime_error("Input data should be a 1D array.");
     }
 
-    // Create a pointer to the data
     const float *data_ptr = static_cast<float *>(buf.ptr);
     size_t size = buf.size;
 
@@ -30,7 +29,7 @@ std::vector<float> computeEMA(const pybind11::array_t<float> &data_array, int sp
     }
 
     std::vector<float> ema(size);
-    float alpha = 2.0f / (span + 1);
+    float alpha = 2.0f / (lookback + 1);
 
     ema[0] = data_ptr[0];
 
@@ -44,6 +43,5 @@ std::vector<float> computeEMA(const pybind11::array_t<float> &data_array, int sp
 
 PYBIND11_MODULE(ema_module, m)
 {
-    m.def("computeEMA", &computeEMA, "A function that computes the Exponential Moving Average (EMA)",
-          pybind11::arg("data"), pybind11::arg("span"));
+    m.def("computeEMA", &computeEMA, "A function that computes the Exponential Moving Average (EMA)", pybind11::arg("data"), pybind11::arg("lookback"));
 }
