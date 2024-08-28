557eddf5156e4545b87ae461def38d9874465b49
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-28 04:19:28 +0200

    Added constants file

---
+++ TraderBot/src/constants.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from enum import StrEnum
+from pathlib import Path
+
+
+@dataclass
+class Paths_:
+    data = Path("data")
+
+
+class Symbol(StrEnum):
+    EURUSD = "EURUSD"
+    GBPUSD = "GBPUSD"
+    USDCAD = "USDCAD"
+    USDCHF = "USDCHF"
+    USDJPY = "USDJPY"
+
+
+class CandleTimeframe(StrEnum):
+    M1 = "1m"
+    M5 = "5m"
+    H1 = "1h"
+    D1 = "1d"
+
+    def to_pandas_timeframe(self) -> str:
+        match self:
+            case CandleTimeframe.M1:
+                return "1min"
+            case CandleTimeframe.M5:
+                return "5min"
+            case CandleTimeframe.H1:
+                return "1h"
+            case CandleTimeframe.D1:
+                return "1D"
+            case _:
+                raise NotImplementedError(
+                    f"{self=} not yet supported for 'to_pandas_timeframe'."
+                )
+
+    @staticmethod
+    def from_pandas_timeframe(pandas_timeframe: str) -> "CandleTimeframe":
+        match pandas_timeframe:
+            case "1min":
+                return CandleTimeframe.M1
+            case "5min":
+                return CandleTimeframe.M5
+            case "1h":
+                return CandleTimeframe.H1
+            case "1D":
+                return CandleTimeframe.D1
+            case _:
+                raise ValueError(f"{pandas_timeframe=} is not supported!")
+
+
+@dataclass
+class DBTables:
+    ticks = "ticks"
+    candles = "candles"
