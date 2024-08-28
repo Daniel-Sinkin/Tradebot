9fccb0bf52524add78535c038912444fd01820ad
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-28 19:52:37 +0200

    Moved TraderBot into a src folder

--- src/TraderBot/setup.py
+++ src/TraderBot/setup.py
@@ -4,11 +4,11 @@ setup(
     name="TraderBot",
     version="0.1.0",
     author="Daniel Sinkin",
-    author_email="danielsinkin97@gmail.com",  # replace with your actual email
+    author_email="danielsinkin97@gmail.com",
     description="A bot for trading with different currency pairs. Intended as a Portfolio Project.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Daniel-Sinkin/Tradebot",  # replace with your actual repository URL
+    url="https://github.com/Daniel-Sinkin/Tradebot",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
@@ -17,14 +17,12 @@ setup(
         "numpy>=1.19.0",
         "scikit-learn>=0.24.0",
         "matplotlib>=3.2.0",
-        # add other dependencies your project requires
     ],
     extras_require={
         "dev": [
             "pytest>=6.0.0",
             "black>=22.0.0",
             "flake8>=3.8.0",
-            # add other development dependencies
         ],
     },
     classifiers=[
@@ -37,7 +35,7 @@ setup(
     python_requires=">=3.8",
     entry_points={
         "console_scripts": [
-            "traderbot=TraderBot.main:main",  # replace `main` with the actual callable
+            "traderbot=TraderBot.main:main",
         ],
     },
 )
